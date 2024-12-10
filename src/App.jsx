3a08684ee4144b52040e654d9db64f33a7102b82
TizianoN <hello@tizianonicolai.com>
2024-12-10 11:58:25 +0100

    feat: react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-// import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// layout
+import DefaultLayout from './layouts/DefaultLayout';
+
+// pages
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
-    <>
-      <div className="container">
-        <h1 className="mt-5">Hello World</h1>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route Component={DefaultLayout}>
+          <Route path="/" Component={HomePage} />
+          <Route path="/about" Component={AboutPage} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
